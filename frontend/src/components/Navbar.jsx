b955c6358e917fc7ccb332fb1e384e4e80042509
Prashant Varshney <prashantvarshney4321@gmail.com>
2025-07-11 00:02:03 +0530

    update navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -67,12 +67,12 @@ const Navbar = () => {
 
 
         </div>
-        <Link to="cart" className="relative">
+        {token && <Link to="cart" className="relative">
           <img src={assets.cart_icon} className="w-5 min-w-5" alt="" />
           <p className="absolute right-[-5px] bottom-[-5px] w-4 text-center leading-4 bg-black text-white aspect-square rounded-full text-[8px]">
             {getCartCount()}
           </p>
-        </Link>
+        </Link>}
 
         <img
           onClick={() => {
